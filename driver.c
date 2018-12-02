545aa5e5dbde7ae4185abd00650fad70e6f59424
sumitgaur93 <sumitgaur93@gmail.com>
2018-12-02 21:42:47 +0530

    removed unused comment

--- driver.c
+++ driver.c
@@ -27,7 +27,6 @@ static int etx_open(struct inode *inode, struct file *file);
 static int etx_release(struct inode *inode, struct file *file);
 static ssize_t etx_read(struct file *filp, char __user *buf, size_t len,loff_t * off);
 static ssize_t etx_write(struct file *filp, const char *buf, size_t len, loff_t * off);
-//void led(unsigned long a);
 
 static struct file_operations fops =
 {
